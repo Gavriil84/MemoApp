dcd5ec1dd7a1630d25ca9eb5aca3cb7b191f9877
Gavriil84 <gabwt.84@gmail.com>
2022-05-26 23:03:33 -0700

    Create MemoEdit & Create screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import MemoCreateScreen from './src/screens/MemoCreatScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 
 export default function App() {
   return (
 
-    <MemoListScreen />
+    <MemoCreateScreen />
   );
 }
 
